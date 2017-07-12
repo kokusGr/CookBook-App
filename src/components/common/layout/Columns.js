42f6987325aa68a04d6a51e22949a487de26c053
Konrad <gren.konrad@gmail.com>
2017-07-12 09:36:42 +0200

    Added custom spacing and number of columns for columns layout

--- src/components/common/layout/Columns.js
+++ src/components/common/layout/Columns.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
 const Columns = styled.div.attrs({
-  spacing: props => props.spacing || '1.5rem'
+  spacing: props => props.spacing || '1.5rem',
+  cols: props => props.cols || '3'
 })`
-  columns: 3;
+  columns: ${props => props.cols};
   column-gap: ${props => props.spacing};
   width: 100%;
 
