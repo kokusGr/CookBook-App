514bbbc8d90b9ac1a547abc5dfaf24933bfc0c60
Konrad <gren.konrad@gmail.com>
2017-08-01 16:46:23 +0200

    Added preloader

--- src/components/ShowRecipe/Recipe.js
+++ src/components/ShowRecipe/Recipe.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import MdCake from 'react-icons/lib/md/cake';
 import MdLocalCafe from 'react-icons/lib/md/local-cafe';
+import BubblePreloader from 'react-bubble-preloader';
 import Wrapper from '../common/Wrapper';
 import Image from '../common/Image';
 import FlexBox from '../common/FlexBox';
@@ -21,54 +22,58 @@ const Ingridiento = ListItem.extend`
   width: 100%;
 `;
 
-const Recipe = ({ recipe }) => (
-  <Wrapper>
-    <FlexBox row>
-      <Column width="40%" recipeImage>
-        <Image noPadding block src={recipe.mainImage.src} />
-      </Column>
-      <Column width="60%" recipeTitle>
-        <Heading1>{recipe.name}</Heading1>
-        <Text>{recipe.description}</Text>
-      </Column>
-    </FlexBox>
-    <FlexBox row>
-      <Column width="31%">
-        <ul>
-          {recipe.ingridients.map(ingridient => (
-            <Ingridiento key={ingridient.id}>{ingridient.text}</Ingridiento>
-          ))}
-        </ul>
-      </Column>
-      <Column width="65%">
-        <Heading2>Directionoro</Heading2>
-        <OrderedList>
-          {recipe.directions.map(direction => (
-            <ListItem direction key={direction.id}>{direction.text}</ListItem>
-          ))}
-        </OrderedList>
-      </Column>
-    </FlexBox>
-    <FlexBox row>
-      <Column width="100%" alignCenter>
-        <Text info>
-          <MdCake />Kcal per 100g: <span>{recipe.kcal}</span>
-        </Text>
-        <Text info>
-          <MdLocalCafe />Number of Servings: <span>{recipe.servings}</span>
-        </Text>
-      </Column>
-    </FlexBox>
-    <FlexBox row>
-      <FlexBox noLayout>
-        {recipe.images.map(image => (
-          <ImageContainer key={image.id} src={image.src} />
-        ))}
+const Recipe = ({ recipe }) => {
+  if (recipe === undefined) {
+    return <BubblePreloader />;
+  }
+  return (
+    <Wrapper>
+      <FlexBox row>
+        <Column width="40%" recipeImage>
+          <Image noPadding block src={recipe.mainImage.src} />
+        </Column>
+        <Column width="60%" recipeTitle>
+          <Heading1>{recipe.name}</Heading1>
+          <Text>{recipe.description}</Text>
+        </Column>
       </FlexBox>
-    </FlexBox>
-  </Wrapper>
-);
-
+      <FlexBox row>
+        <Column width="31%">
+          <ul>
+            {recipe.ingridients.map(ingridient => (
+              <Ingridiento key={ingridient.id}>{ingridient.text}</Ingridiento>
+            ))}
+          </ul>
+        </Column>
+        <Column width="65%">
+          <Heading2>Directionoro</Heading2>
+          <OrderedList>
+            {recipe.directions.map(direction => (
+              <ListItem direction key={direction.id}>{direction.text}</ListItem>
+            ))}
+          </OrderedList>
+        </Column>
+      </FlexBox>
+      <FlexBox row>
+        <Column width="100%" alignCenter>
+          <Text info>
+            <MdCake />Kcal per 100g: <span>{recipe.kcal}</span>
+          </Text>
+          <Text info>
+            <MdLocalCafe />Number of Servings: <span>{recipe.servings}</span>
+          </Text>
+        </Column>
+      </FlexBox>
+      <FlexBox row>
+        <FlexBox noLayout>
+          {recipe.images.map(image => (
+            <ImageContainer key={image.id} src={image.src} />
+          ))}
+        </FlexBox>
+      </FlexBox>
+    </Wrapper>
+  );
+};
 Recipe.propTypes = {
   recipe: PropTypes.shape({})
 };
